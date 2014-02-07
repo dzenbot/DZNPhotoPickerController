306bf37cdcea041a59b7bf61b3d0ddcbdb15f88b
dzenbot <iromero@dzen.cl>
2014-02-07 10:01:21 -0300

    Added foundations for unit tests

--- Examples/Sample/SampleTests/SampleTests.m
+++ Examples/Sample/SampleTests/SampleTests.m
@@ -7,9 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "DZNPhotoPickerConstants.h"
+#import "DZNPhotoPickerController.h"
+
+static NSBundle *_testTargetBundle;
 
 @interface SampleTests : XCTestCase
-
 @end
 
 @implementation SampleTests
@@ -17,18 +20,49 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    NSString *bundleIdentifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+    _testTargetBundle = [NSBundle bundleWithIdentifier:bundleIdentifier];
+    
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
+- (NSDictionary *)JSONObjectForService:(DZNPhotoPickerControllerService)service
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertNotNil(_testTargetBundle, @"path : %@", _testTargetBundle);
+
+    NSString *path = [_testTargetBundle pathForResource:[NSStringFromServiceType(service) lowercaseString] ofType:@"json"];
+    XCTAssertNotNil(path, @"path : %@", path);
+    
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    XCTAssertNotNil(data, @"data : %@", data);
+    
+    id object = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions|NSJSONWritingPrettyPrinted error:nil];
+    XCTAssertNotNil(data, @"object : %@", object);
+    
+    return object;
 }
+
+- (void)test500pxSearch
+{
+    NSDictionary *object = [self JSONObjectForService:DZNPhotoPickerControllerService500px];
+    XCTAssertNotNil(object, @"500px object : %@", object);
+
+    
+}
+
+- (void)testFlickrSearch
+{
+    NSDictionary *object = [self JSONObjectForService:DZNPhotoPickerControllerServiceFlickr];
+    XCTAssertNotNil(object, @"Flickr object : %@", object);
+    
+    
+}
+
+
 
 @end
