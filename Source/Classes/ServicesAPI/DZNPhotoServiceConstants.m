1adf0b00e6f466c42b572aef9bc7afce2ab760b9
dzenbot <iromero@dzen.cl>
2014-05-29 11:36:51 -0400

    Fixes Flickr 403 error. Inspired from @kaojohnny commit b5e0846

--- Source/Classes/ServicesAPI/DZNPhotoServiceConstants.m
+++ Source/Classes/ServicesAPI/DZNPhotoServiceConstants.m
@@ -28,7 +28,7 @@ NSURL *baseURLForService(DZNPhotoPickerControllerServices service)
 {
     switch (service) {
         case DZNPhotoPickerControllerService500px:              return [NSURL URLWithString:@"https://api.500px.com/v1"];
-        case DZNPhotoPickerControllerServiceFlickr:             return [NSURL URLWithString:@"http://api.flickr.com/services/rest/"];
+        case DZNPhotoPickerControllerServiceFlickr:             return [NSURL URLWithString:@"https://api.flickr.com/services/rest/"];
         case DZNPhotoPickerControllerServiceInstagram:          return [NSURL URLWithString:@"https://api.instagram.com/v1/"];
         case DZNPhotoPickerControllerServiceGoogleImages:       return [NSURL URLWithString:@"https://www.googleapis.com/customsearch/v1/"];
         default:                                                return nil;
