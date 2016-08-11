5f138bc019c13c778e52b7e27bf1b319ce6a4e07
Ahmed BOUCHFAA <abouchfaa@gmail.com>
2016-08-11 22:57:05 +0100

    back to origin

---
+++ Examples/PhotoEditor/Pods/Target Support Files/Pods-PhotoEditor/Pods-PhotoEditor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoEditor : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoEditor
+@end
---
+++ Examples/PhotoPicker/Pods/Target Support Files/Pods-PhotoPicker/Pods-PhotoPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoPicker : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoPicker
+@end
